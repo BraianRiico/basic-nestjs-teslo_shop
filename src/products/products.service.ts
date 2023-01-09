da9cbce2bd8c3b6b37ff7acd9924149867063091
Braian Rico <daviidriico@gmail.com>
2023-01-09 11:16:27 -0500

    [126.Manejo de errores] - Manejando errores al momento de persistir en base de datos (pendiente por terminar)

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm'; 
 import { CreateProductDto } from './dto/create-product.dto';
@@ -7,6 +7,7 @@ import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+  private readonly logger = new Logger('ProductService');
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
@@ -20,9 +21,7 @@ export class ProductsService {
       return product;
       
     } catch (error) {
-      console.log(error)
-      throw new InternalServerErrorException('Ayuda');
-      
+      this.handleDBExceptions(error);
     }  
   }
 
@@ -40,5 +39,13 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private handleDBExceptions( error:any ) {
+    if( error.code === '23505' ) throw new BadRequestException(error.detail);
+
+      this.logger.error(error);
+      throw new InternalServerErrorException('Unexpected error, check serve logs');
+      
   }
 }

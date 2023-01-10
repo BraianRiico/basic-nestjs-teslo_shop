60b6c781282651783c2810826321d623cddeac83
Braian Rico <daviidriico@gmail.com>
2023-01-09 21:04:59 -0500

    [130.buscar por Slug o UUID] - Reestructurando consulta de buscar por id para poder buscar tambien por slug

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
+import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class ProductsService {
@@ -38,12 +39,17 @@ export class ProductsService {
     });
   }
 
-  async findOne( id: string ) {
+  async findOne( term: string ) {
 
-    const product = await this.productRepository.findOneBy({ id });
+    let product: Product;
+
+    if ( isUUID(term) ){
+      product = await this.productRepository.findOneBy({ id: term });
+    } else {
+      product = await this.productRepository.findOneBy({ slug: term });
+    }
     
-    if(!product) throw new NotFoundException(`Product with id ${ id } not found`);
-    
+    if(!product) throw new NotFoundException(`Product with ${ term } not found`);
     return product;
   }
 

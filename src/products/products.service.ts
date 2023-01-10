12969b093ef57559f93249493dcd1c765fd4f966
Braian Rico <daviidriico@gmail.com>
2023-01-09 21:26:25 -0500

    [131.QueryBuilder] - Estructurando consultas sql de postgres directamente en el metodo

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -46,7 +46,11 @@ export class ProductsService {
     if ( isUUID(term) ){
       product = await this.productRepository.findOneBy({ id: term });
     } else {
-      product = await this.productRepository.findOneBy({ slug: term });
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder.where( 'UPPER(title) =:title or slug=:slug',{
+        title:  term.toUpperCase(), //utilizar el metodo touppercase es opción para ser más flexibles en las consultas
+        slug:   term.toLowerCase(), //utilizar el metodo tolowercase es opción para ser más flexibles en las consultas
+      }).getOne();
     }
     
     if(!product) throw new NotFoundException(`Product with ${ term } not found`);

f88fd44fb5b10299f5a9cae680b05c23dc5a7fca
Braian Rico <daviidriico@gmail.com>
2023-01-17 20:44:58 -0500

    [140. ProductImage Entity] - Creando tabla para almacenar imagenes de los productos (sin relaciónes)

---
+++ src/products/entities/index.ts
@@ -0,0 +1,7 @@
+//Archivo de barril, que se utiliza para exportar varias clases de una misma entidad
+//Ojo debe llamarse index.ts
+
+export { ProductImage } from "./product-image.entity";
+export { Product } from "./product.entity";
+
+

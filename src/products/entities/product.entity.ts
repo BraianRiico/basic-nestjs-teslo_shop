6598b6d1f21e3dba05d97c178c91c8359b04fa94
Braian Rico <daviidriico@gmail.com>
2023-01-17 21:09:55 -0500

    [141. OneToMany y ManyToOne] - Creación de campo foraneo de product en la tabla de productImages con su respectiva relación en las dos tablas

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,5 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ProductImage } from "./";
 
 @Entity()
     export class Product {
@@ -45,8 +46,15 @@ import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } fr
     })
     tags: string[]
     
-    //tags
-    //images
+    @OneToMany(
+        () => ProductImage,
+        (productImage) => productImage.product,
+        { cascade: true }
+    )
+    images?: ProductImage[];
+    
+
+
     @BeforeInsert()
     checkSlugInsert() {
         if ( !this.slug ){

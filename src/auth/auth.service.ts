cfaceac295e9bb72f4be521ff6020628648f9306
Braian Rico <daviidriico@gmail.com>
2023-01-19 21:47:14 -0500

    [169. Login de usuario] - se realizan las validaciones de correo y contraseña para logearse - falta JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,12 @@
 import { Repository } from 'typeorm';
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import * as bcrypt from 'bcrypt';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import { LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -33,6 +35,23 @@ export class AuthService {
     } catch (error) {
       this.handleDBErrors( error );
     }
+  }
+
+  async login( loginUserDto: LoginUserDto ){
+
+    const { password, email } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true }
+     });
+
+     if ( !user ) throw new UnauthorizedException('Credentials are not valid (email)');
+
+     if ( !bcrypt.compareSync( password, user.password) ) throw new UnauthorizedException('Credentials are not valid (password)');
+
+    return user;
+    //TODO retornar JWT de acceso
   }
 
   private handleDBErrors( error: any ): never {

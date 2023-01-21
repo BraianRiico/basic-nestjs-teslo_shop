cc6ef20aac0c29436f2663b5b90b8a6a8606ec23
Braian Rico <daviidriico@gmail.com>
2023-01-20 20:54:41 -0500

    [177. Custom property decorator - GetUser]

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,7 +3,9 @@ import { AuthGuard } from '@nestjs/passport';
 
 
 import { AuthService } from './auth.service';
+import { GetUser } from './decorators/get-user.decorator';
 import { CreateUserDto, LoginUserDto } from './dto';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -21,10 +23,14 @@ export class AuthController {
 
   @Get('private')
   @UseGuards( AuthGuard() )
-  testingPrivateRoute() {
+  testingPrivateRoute(
+    @GetUser() user: User
+  ) {
+
     return {
       ok: true,
       message: 'Hola Mundo Private',
+      user,
     }
   }
 }
